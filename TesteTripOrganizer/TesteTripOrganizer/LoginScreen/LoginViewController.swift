e7d9aed59b1aadc6296305455e85ecd1994e778f
Felipe Augusto <augusto_felipe@icloud.com>
2023-03-06 17:20:22 -0300

    Botao adicionado

---
+++ TesteTripOrganizer/TesteTripOrganizer/LoginScreen/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  TesteTripOrganizer
+//
+//  Created by Felipe Augusto Correia on 06/03/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+}
